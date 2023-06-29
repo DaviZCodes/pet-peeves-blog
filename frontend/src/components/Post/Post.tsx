685424361cced3641f7bf70fc8e8503e650dc1a9
DaviZCodes <davi.bestguy@gmail.com>
2023-06-29 00:11:04 -0400

    restricted access to post only for logged in users

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,17 +1,36 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useContext} from "react";
 import axios from "axios";
 import "./Post.css"
+import { UserContext } from "../UserContext/UserContext";
+import { useNavigate } from "react-router";
 
 function Post() {
 
+    //user can only access /post if logged in
+    const { userInfo } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    function createPost() {
+        
+    }
+
+    //user can only access /post if logged in
     useEffect(() =>{
+        //change title of tab
         document.title = "Post!"
+
+        if (!userInfo) {
+            navigate("/login");
+        }
     }, [])
 
     return (
         <div className="post">
             <h1>Create a Post</h1>
             <textarea></textarea>
+            <div className="button-container">
+                <button id = "submit" onClick={createPost}>Post</button>
+            </div>
         </div>
     );
 }
