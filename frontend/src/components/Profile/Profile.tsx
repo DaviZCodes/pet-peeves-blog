62b5cb8e3a1df7ab4b626e1b61ba7782408d16af
DaviZCodes <davi.bestguy@gmail.com>
2023-06-29 22:23:48 -0400

    changes

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,13 +1,16 @@
 import React, {useState, useEffect, useContext} from "react";
 import "./Profile.css"
 import { UserContext } from '../UserContext/UserContext';
+import sadCat from "./images/cat sad.png"
 
 function Profile() {
     const {userInfo, setUserInfo} = useContext(UserContext);
     const [noPostsText, setNoPostsText] = useState<string>("You currently have no posts. Once you create a post, it will appear here.")
+    const [showCat, setShowCat] = useState<boolean>(true);
 
     // useEffect(() => {
     //     setNoPostsText("");
+    //     setShowCat(false);
     // }, [])
 
     return (
@@ -15,6 +18,11 @@ function Profile() {
             <h1>Welcome back, "{userInfo}".</h1>
 
             <h2>Below are your posts:</h2>
+            {showCat && (
+                <>
+            <img src = {sadCat} alt = "Crying cat" id = "sad-cat"></img>
+            </>
+            )}
             <p id = "no-posts">{noPostsText}</p>
         </div>
     );
