de1c55d2a0783e5f3ffbd681206e286c28b82979
DaviZCodes <davi.bestguy@gmail.com>
2023-06-30 22:21:58 -0400

    added some css

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -14,27 +14,30 @@ function Home() {
         <div className="home">
             <div className="posts-container">
                 <div className="post-home">
-                    <h1>My dog is WAY too SPOILED!</h1>
+                    <h1 id = "post-header">My dog is WAY too SPOILED!</h1>
+                    <p id = "poster">Sophie Smith  &nbsp;&nbsp; Jan 1, 2023</p>
                     <div className="image">
-                        <img src = {funnyDog}></img>
+                        <img src = {funnyDog} id = "home-img"></img>
                     </div>
-                    <p>My dog is an alcoholic.</p>
+                    <p id = "post-desc">My dog is an alcoholic.</p>
                 </div>
 
                 <div className="post-home">
-                    <h1>Why does my parrot lie about everything?</h1>
+                    <h1 id = "post-header">Why does my parrot lie about everything?</h1>
+                    <p id = "poster">John Thoe &nbsp;&nbsp; June 30, 2023</p>
                     <div className="image">
-                     <img src = {parrot}></img>
+                     <img src = {parrot} id = "home-img"></img>
                     </div>
-                    <p>My parrot likes to lie about everything?</p>
+                    <p id = "post-desc">My parrot likes to lie about everything?</p>
                 </div>
 
-                <div className="post">
-                    <h1>How do you pet a shark?</h1>
+                <div className="post-home">
+                    <h1 id = "post-header">How do you pet a shark?</h1>
+                    <p id = "poster">Saad Ali &nbsp;&nbsp; Mar 23, 2023</p>
                     <div className="image">
-                        <img src = {shark}></img>
+                        <img src = {shark} id = "home-img"></img>
                     </div>
-                    <p>Hey. I am a millionaire from Dubai. I have bought a giant 10,000 square feet aquarium. Is it possible to pet my shark?</p>
+                    <p id = "post-desc">Hey. I am a millionaire from Dubai. I have bought a giant 10,000 square feet aquarium. Is it possible to pet my shark?</p>
                 </div>
             </div>
         </div>
