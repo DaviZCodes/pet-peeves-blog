0b80f39ec0e105e527fb28f49299e963b5aea22a
DaviZCodes <davi.bestguy@gmail.com>
2023-07-11 10:51:15 -0400

    added environment variables

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -1,11 +1,11 @@
 import crypto from "crypto";
-
-const SECRET = "PetLoggerBlog19MyBdayInTwoDays" //I'm going to be 20 years old in 2 days! June 26 btw!
+require("dotenv").config();
+const SECRET = (process.env.SECRET);
 
 export const random = () => {
     return crypto.randomBytes(128).toString("base64");
 }
 
 export const authentication = (salt : string, password : string) => {
-    return crypto.createHmac("sha256", [salt, password].join("/")).update(SECRET).digest("hex");
+    return crypto.createHmac("sha256", [salt, password].join("/")).update(SECRET!).digest("hex");
 }
