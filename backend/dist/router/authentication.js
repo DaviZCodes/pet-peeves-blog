9c5a35440671a26e9c419b29d33eb6892bf6a564
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2023-07-12 11:36:55 -0400

    Delete backend/dist directory

--- backend/dist/router/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("../controllers/authentication");
-exports.default = (router) => {
-    router.post("/register", authentication_1.register);
-    router.post("/login", authentication_1.login);
-};
-//# sourceMappingURL=authentication.js.map
