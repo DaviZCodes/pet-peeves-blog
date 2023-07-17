46f5444ce1bdd44946daa452985fdae96546b254
davi z <davi.bestguy@gmail.com>
2023-07-17 11:05:03 -0400

    css better styling

--- frontend/src/components/HomePost/HomePost.tsx
+++ frontend/src/components/HomePost/HomePost.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { format } from "date-fns";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface HomePostProps {
     _id: string;
@@ -23,27 +24,26 @@ interface HomePostProps {
       content.length > 306 ? content.slice(0, 306) + "..." : content;
   
     const contentToShow = content.length > 306 ? truncatedContent : content;
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/posts/${_id}`);
+    }
 
     return (
-        <div className="post-home">
-        <div className="image">
-            <Link to = {`/posts/${_id}`}>
-            <img src={"http://localhost:8019/" + cover} id="home-img" alt="Post Cover" />
-            </Link>
-        </div>
-        <div className="post-content">
-            <Link to = {`/posts/${_id}`}>
-                <h1>{title}</h1>
-            </Link>
-            <p className="info">
-                <a className="author">{author}</a>
-                <time>{format(new Date(createdAt), "MMM d, yyyy")}</time>
-            </p>
-            <Link to = {`/posts/${_id}`}>
-                <p id="summary" dangerouslySetInnerHTML={{ __html: contentToShow  }}/>
-                {content.length > 600 && <p id = "read-more"> Read more</p>}
-            </Link>
-        </div>
+        <div className="post-home" onClick={handleClick} style = {{cursor: "pointer"}}>
+            <div className="image">
+                <img src={"http://localhost:8019/" + cover} id="home-img" alt="Post Cover" />
+            </div>
+            <div className="post-content">
+                    <h1>{title}</h1>
+                <p className="info">
+                    <a className="author">{author}</a>
+                    <time>{format(new Date(createdAt), "MMM d, yyyy")}</time>
+                </p>
+                    <p id="summary" dangerouslySetInnerHTML={{ __html: contentToShow  }}/>
+                    {content.length > 600 && <p id = "read-more"> Read more</p>}
+            </div>
         </div>
     );
 };
