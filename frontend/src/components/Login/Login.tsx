03f5c15c63401754a18903036c42b0e87fcbf7be
DaviZCodes <davi.bestguy@gmail.com>
2023-07-27 14:50:20 -0400

    added show password for register/login

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,83 +1,107 @@
-import {useState, useEffect, useContext} from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.scss";
 import axios from "axios";
 import { UserContext } from "../UserContext/UserContext";
 
 function Login() {
+  //useState for username and passwords
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [wrongPasswordText, setWrongPasswordText] = useState<string>("");
+  const navigate = useNavigate();
+  const { setUserInfo, userInfo } = useContext(UserContext);
 
-    //useState for username and passwords
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const[wrongPasswordText, setWrongPasswordText] = useState<string>("");
-    const navigate = useNavigate();
-    const { setUserInfo, userInfo } = useContext(UserContext);
+  //waiting for submit button
+  async function submit(element: { preventDefault: () => void }) {
+    element.preventDefault();
+    try {
+      //this "user" and "pass" is only for testing purposes to test the frontend, to actually
+      //function, we must use cookie parser from the backend. Delete this after
+      if (username === "user" && password == "pass") {
+        localStorage.setItem("token", "testing-purposes");
+        setUserInfo(username);
+        navigate("/");
+      }
+      //delete above
 
-    //waiting for submit button
-    async function submit(element: { preventDefault: () => void; }) {
-        element.preventDefault();
-        try {
-
-            //this "user" and "pass" is only for testing purposes to test the frontend, to actually
-            //function, we must use cookie parser from the backend. Delete this after
-            if (username === "user" && password == "pass") {
-                localStorage.setItem("token", "testing-purposes");
-                setUserInfo( username );
-                navigate("/");
-            }
-            //delete above
-
-            const response = await axios.post("http://localhost:8019/login", {
-              username,
-              password,
-            }, {
-                withCredentials: true, //allow cookies because of authentication middleware
-            });
-            setUserInfo(username);
-            navigate("/");
+      const response = await axios.post(
+        "http://localhost:8019/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true, //allow cookies because of authentication middleware
         }
-
-        catch (error) { 
-            setWrongPasswordText("Wrong username or password.");
-            console.log(error);
-        }
+      );
+      setUserInfo(username);
+      navigate("/");
+    } catch (error) {
+      setWrongPasswordText("Wrong username or password.");
+      console.log(error);
     }
+  }
 
-    //change tab title
-    document.title = "Login!"
+  //change tab title
+  document.title = "Login!";
 
+  //if logged in, automatically go to "/post"
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/post");
+    }
+  }, [userInfo]);
 
-    //if logged in, automatically go to "/post"
-    useEffect(() => {
-        if (userInfo){
-            navigate("/post");
-        }
-    }, [userInfo])
+  return (
+    <div className="login">
+      <div className="container">
+        <h1 id="login-text">Login</h1>
+        <form action="POST">
+          <input
+            type="username"
+            id="username"
+            placeholder="username"
+            onChange={(element) => {
+              setUsername(element.target.value);
+            }}
+          ></input>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder="password"
+            onChange={(element) => {
+              setPassword(element.target.value);
+            }}
+          />
+          <div
+            className="password-toggle"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? "Hide password" : "Show password"}
+          </div>
+          <input type="submit" onClick={submit} id="submit"></input>
+        </form>
 
-    return (
-        <div className="login">
-            <div className="container">
-            <h1 id = "login-text">Login</h1>
-            <form action="POST">
-                <input type="username" id = "username" placeholder="username" onChange={(element) => {setUsername(element.target.value);}}></input>
-                <input type="password" id = "password" placeholder="password" onChange={(element) => {setPassword(element.target.value);}}></input>
-                <input type="submit" onClick={submit} id = "submit"></input>
-            </form>
+        <p id="wrong-password">{wrongPasswordText}</p>
 
-            <p id = "wrong-password">{wrongPasswordText}</p>
-        
-            <Link to = "/register" id = "register">
-            <h3 id = "question">Don't have an account?</h3>
-            </Link>
-            <Link to = "/register" id = "register-text"> Register </Link>
+        <Link to="/register" id="register">
+          <h3 id="question">Don't have an account?</h3>
+        </Link>
+        <Link to="/register" id="register-text">
+          Register
+        </Link>
 
-            {/* testing frontend */}
-            <br></br>
-            <br></br>
-            <p style = {{fontSize: 10}}>username: "user" and password: "pass" for frontend testing</p>
-            </div>
-        </div>
-    );
+        {/* testing frontend */}
+        <br></br>
+        <br></br>
+        <p style={{ fontSize: 10 }}>
+          username: "user" and password: "pass" for frontend testing
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -1,48 +1,73 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Register.scss"
+import "./Register.scss";
 
 function Register() {
-    //useState for username and passwords
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const[cannotRegister, setcannotRegister] = useState("");
-    const navigate = useNavigate();
+  //useState for username and passwords
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [cannotRegister, setcannotRegister] = useState<string>("");
+  const navigate = useNavigate();
 
-    //waiting for submit button
-    async function submit(element: { preventDefault: () => void; }) {
-        element.preventDefault();
+  //waiting for submit button
+  async function submit(element: { preventDefault: () => void }) {
+    element.preventDefault();
 
-        try {
-            await axios.post("http://localhost:8019/register", {
-                username, password
-            })
-            navigate("/login");
-        }
-
-        catch (error) {
-            setcannotRegister("Use another username or password.")
-            console.log(error);
-        }
+    try {
+      await axios.post("http://localhost:8019/register", {
+        username,
+        password,
+      });
+      navigate("/login");
+    } catch (error) {
+      setcannotRegister("Use another username or password.");
+      console.log(error);
     }
+  }
 
-    //change tab title
-    document.title = "Register!"
+  //change tab title
+  document.title = "Register!";
 
-    return (
-        <div className="register-page">
-            <div className="register-container">
-            <h1>Register</h1>
-            <form action="POST">
-                <input type="username" id = "username" placeholder="username" value = {username} onChange={(element) => {setUsername(element.target.value);}}></input>
-                <input type="password" id = "password" placeholder="password" value = {password} onChange={(element) => {setPassword(element.target.value);}}></input>
-                <button id = "join-now" onClick={submit}> Join Now </button>
-            </form>
-            <p id = "cannotRegister">{cannotRegister}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="register-page">
+      <div className="register-container">
+        <h1>Register</h1>
+        <form action="POST">
+          <input
+            type="username"
+            id="username"
+            placeholder="username"
+            value={username}
+            onChange={(element) => {
+              setUsername(element.target.value);
+            }}
+          ></input>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder="password"
+            value={password}
+            onChange={(element) => {
+              setPassword(element.target.value);
+            }}
+          />
+          <div
+            className="password-toggle"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? "Hide password" : "Show password"}
+          </div>
+          <button id="join-now" onClick={submit}>
+            {" "}
+            Join Now{" "}
+          </button>
+        </form>
+        <p id="cannotRegister">{cannotRegister}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
