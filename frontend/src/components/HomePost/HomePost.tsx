bc810e277cec542980104c2ffa5dfbf6448f7ed9
DaviZCodes <davi.bestguy@gmail.com>
2023-08-07 12:48:15 -0400

    DOMPurify to sanitize innerhtml

--- frontend/src/components/HomePost/HomePost.tsx
+++ frontend/src/components/HomePost/HomePost.tsx
@@ -1,52 +1,64 @@
 import React from "react";
 import { format } from "date-fns";
-import { Link } from "react-router-dom";
+import DOMPurify from "dompurify";
 import { useNavigate } from "react-router-dom";
 
 interface HomePostProps {
-    _id: string;
-    title: string;
-    createdAt: string;
-    content: string;
-    cover: string;
-    author: string;
-  } 
+  _id: string;
+  title: string;
+  createdAt: string;
+  content: string;
+  cover: string;
+  author: string;
+}
 
-  const HomePost: React.FC<HomePostProps> = ({
-    _id,
-    title,
-    createdAt,
-    content,
-    cover,
-    author,
-  }) => {
-    const truncatedContent =
-      content.length > 306 ? content.slice(0, 306) + "..." : content;
-  
-    const contentToShow = content.length > 306 ? truncatedContent : content;
-    const navigate = useNavigate();
+const HomePost: React.FC<HomePostProps> = ({
+  _id,
+  title,
+  createdAt,
+  content,
+  cover,
+  author,
+}) => {
+  const truncatedContent =
+    content.length > 306 ? content.slice(0, 306) + "..." : content;
 
-    const handleClick = () => {
-        navigate(`/posts/${_id}`);
-    }
+  const contentToShow = content.length > 306 ? truncatedContent : content;
+  const navigate = useNavigate();
 
-    return (
-        <div className="post-home" onClick={handleClick} style = {{cursor: "pointer"}}>
-            <div className="image">
-                <img src={"http://localhost:8019/" + cover} id="home-img" alt="Post Cover" />
-            </div>
-            <div className="post-content">
-                    <h1>{title}</h1>
-                <p className="info">
-                    <a className="author">{author}</a>
-                    <time>{format(new Date(createdAt), "MMM d, yyyy")}</time>
-                </p>
-                    <p id="summary" dangerouslySetInnerHTML={{ __html: contentToShow  }}/>
-                    {content.length > 600 && <p id = "read-more"> Read more</p>}
-            </div>
-        </div>
-    );
+  const handleClick = () => {
+    navigate(`/posts/${_id}`);
+  };
+
+  return (
+    <div
+      className="post-home"
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    >
+      <div className="image">
+        <img
+          src={"http://localhost:8019/" + cover}
+          id="home-img"
+          alt="Post Cover"
+        />
+      </div>
+      <div className="post-content">
+        <h1>{title}</h1>
+        <p className="info">
+          <a className="author">{author}</a>
+          <time>{format(new Date(createdAt), "MMM d, yyyy")}</time>
+        </p>
+        <p
+          id="summary"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(contentToShow),
+          }}
+        />
+        {content.length > 600 && <p id="read-more"> Read more</p>}
+      </div>
+    </div>
+  );
 };
-
 
 export default HomePost;
--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,123 +1,137 @@
-import {useState, useEffect, useContext} from "react";
-import "./Profile.scss"
-import { UserContext } from '../UserContext/UserContext';
+import { useState, useEffect, useContext } from "react";
+import "./Profile.scss";
+import { UserContext } from "../UserContext/UserContext";
 import sadCat from "./images/cat sad.png";
 import loadingGif from "./images/spinning loading.gif";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 interface Post {
-    _id: string;
-    title: string;
-    content: string;
-    cover: string;
-    createdAt: string;
-    author: string;
-  }
+  _id: string;
+  title: string;
+  content: string;
+  cover: string;
+  createdAt: string;
+  author: string;
+}
 
 function Profile() {
-    const navigate = useNavigate();
-    const { userInfo } = useContext(UserContext);
-    const [showCatAndPost, setShowCatAndPost] = useState<boolean>(false);
-    const [posts, setPosts] = useState<Post[]>([]);
-    const [fetchingPostsText, setFetchingPostsText] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const { userInfo } = useContext(UserContext);
+  const [showCatAndPost, setShowCatAndPost] = useState<boolean>(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [fetchingPostsText, setFetchingPostsText] = useState<boolean>(true);
 
-   //fetch all of your own posts to profile
-   useEffect(() => {
+  //fetch all of your own posts to profile
+  useEffect(() => {
     const fetchUserPosts = async (): Promise<void> => {
       try {
-        const response = await axios.get(`http://localhost:8019/user-posts/${userInfo!}`);
-      
+        const response = await axios.get(
+          `http://localhost:8019/user-posts/${userInfo!}`
+        );
+
         if (response.status === 200) {
           setPosts(response.data);
-  
+
           if (response.data.length === 0) {
             setTimeout(() => {
-                setShowCatAndPost(true);
-                setFetchingPostsText(false);
-              }, 500);
-
+              setShowCatAndPost(true);
+              setFetchingPostsText(false);
+            }, 500);
           } else {
             setShowCatAndPost(false);
             setFetchingPostsText(false);
           }
         }
-      } 
-      catch (error) {
+      } catch (error) {
         console.log(error);
       }
     };
-  
+
     fetchUserPosts();
   }, [userInfo]);
 
+  useEffect(() => {
+    // Change title of tab
+    document.title = "Profile!";
 
-    useEffect(() => {
-        // Change title of tab
-        document.title = 'Profile!';
-    
-        if (!userInfo) {
-          const timeoutId = setTimeout(() => {
-            navigate('/login');
-          }, 2000);
-    
-          return () => clearTimeout(timeoutId);
-        }
-      }, [userInfo, navigate]);
+    if (!userInfo) {
+      const timeoutId = setTimeout(() => {
+        navigate("/login");
+      }, 2000);
 
-    return (
-        <div className="profile">
-            <h1>Welcome back,&nbsp; 
-                <span id = "profile-username">{userInfo}</span>
-                .
-            </h1>
+      return () => clearTimeout(timeoutId);
+    }
+  }, [userInfo, navigate]);
 
-            <h2>Below are your posts:</h2>
-            <br></br>
-            {fetchingPostsText && (
-                <>
-                <p>Fetching your posts...</p>
-                <div style = {{marginTop: "30px"}}>
-                    <img src = {loadingGif} id = "loading"></img>
+  return (
+    <div className="profile">
+      <h1>
+        Welcome back,&nbsp;
+        <span id="profile-username">{userInfo}</span>.
+      </h1>
+
+      <h2>Below are your posts:</h2>
+      <br></br>
+      {fetchingPostsText && (
+        <>
+          <p>Fetching your posts...</p>
+          <div style={{ marginTop: "30px" }}>
+            <img src={loadingGif} id="loading"></img>
+          </div>
+        </>
+      )}
+      {!showCatAndPost && (
+        <>
+          {posts.map((post, index) => (
+            <div key={post._id}>
+              <h1 id="post-title">{post.title}</h1>
+
+              {userInfo === post.author && (
+                <div className="#edit-post-container">
+                  <Link to={`/edit/${post._id}`} id="edit-post">
+                    Edit Post
+                  </Link>
                 </div>
-                </>
-            )}
-            {!showCatAndPost && (
-                <>
-                    {posts.map((post, index) => (
-                    <div key={post._id}>
-                        <h1 id = "post-title">{post.title}</h1>
+              )}
 
-                        {userInfo === post.author && (
-                            <div className = "#edit-post-container">
-                                <Link to = {`/edit/${post._id}`} id = "edit-post">Edit Post</Link>
-                            </div>
-                        )}
+              <img
+                src={`http://localhost:8019/${post.cover}`}
+                id="profile-post-img"
+              ></img>
 
-                        <img src = {`http://localhost:8019/${post.cover}`} id = "profile-post-img"></img>
+              <div className="post-content">
+                <p
+                  id="summary"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(post.content),
+                  }}
+                />
+              </div>
+              <div className="page-line"></div>
+            </div>
+          ))}
+        </>
+      )}
 
-                        <div className="post-content">
-                            <p id="summary" dangerouslySetInnerHTML={{ __html: post.content }}/>
-                        </div>
-                        <div className="page-line"></div>
-                    </div>
-                    ))}
-                </>
-                )}
+      {showCatAndPost && (
+        <>
+          <Link to="/post">
+            <img src={sadCat} alt="Crying cat" id="sad-cat"></img>
+          </Link>
 
-            {showCatAndPost && (
-                <>
-                <Link to = "/post">
-                    <img src = {sadCat} alt = "Crying cat" id = "sad-cat"></img>
-                </Link>
-                
-                <p id = "no-posts">You currently have no posts. Once you create a&nbsp;<Link to = "/post" id = "post">post </Link>, it will appear here.</p>
-                </>
-            )
-}
-
-        </div>
-    );
+          <p id="no-posts">
+            You currently have no posts. Once you create a&nbsp;
+            <Link to="/post" id="post">
+              post{" "}
+            </Link>
+            , it will appear here.
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
